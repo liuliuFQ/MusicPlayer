b6118957be4c8cba8c81b853e3813af44643f907
Ibokan <ibokan@12-07.local>
2014-04-14 20:38:02 +0800

    Initial Commit

---
+++ MusicPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by Ibokan on 14-4-14.
+//  Copyright (c) 2014年 CainiaoLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MusicPlayerTests/MusicPlayerTests.m
@@ -0,0 +1,34 @@
+//
+//  MusicPlayerTests.m
+//  MusicPlayerTests
+//
+//  Created by Ibokan on 14-4-14.
+//  Copyright (c) 2014年 CainiaoLiu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MusicPlayerTests : XCTestCase
+
+@end
+
+@implementation MusicPlayerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
